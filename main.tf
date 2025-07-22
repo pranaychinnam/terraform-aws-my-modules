6c9e8894185f8b5a82b9fd691842302b4646076d
pranaychinnam <chinnam.pranay949@gmail.com>
2025-07-22 20:54:15 +0530

    changes

--- main.tf
+++ main.tf
@@ -1,20 +1,6 @@
 provider "aws" {
     region = "us-east-1"  
 }
-
-# resource "aws_instance" "foo" {
-#   ami           = "ami-05fa00d4c63e32376" # us-west-2
-#   instance_type = "t2.micro"
-#   tags = {
-#       Name = "TF-jenkins-Instance"
-#   }
-# }
-
-
-# provider "aws" {
-#   region = "ap-south-1"
-# }
-
 resource "aws_instance" "example" {
   ami           = "ami-0cbbe2c6a1bb2ad63"  # Amazon Linux 2023 (Mumbai)
   instance_type = "t2.micro"
